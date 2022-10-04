a657e474abe599d4b3f77108321fa4a20eeb4958
SHnice <108930470+SHnice@users.noreply.github.com>
2022-10-04 19:33:38 +0500

    adding server link

--- src/componentes/AddInstallment.js
+++ src/componentes/AddInstallment.js
@@ -17,7 +17,7 @@ export default function AddInstallment(props) {
         event.preventDefault()
         
         axios
-          .post('http://localhost:8000/paid', {paid:value, id:props.id, price:props.price})
+          .post('https://babasardaarserver.herokuapp.com//paid', {paid:value, id:props.id, price:props.price})
           .then((e)=>{
             props.fetchInstallments()
             props.modal(false)
